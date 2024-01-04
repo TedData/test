a719c286196c4f9ab1839951123b29c7613b8bf6
TedData <144078003+TedData@users.noreply.github.com>
2024-01-04 12:21:14 +1000

    Add files via upload

--- code/createNewWindow.ps1
+++ code/createNewWindow.ps1
@@ -1,102 +1,71 @@
-﻿function Read-InputBoxDialog
-{
-    param (
-        [string]$Prompt1,
-        [string]$Prompt2,
-        [string]$DefaultValue1 = "",
-        [string]$DefaultValue2 = "",
-        [string]$WindowTitle,
-        [System.Windows.Forms.MessageBoxButtons]$Buttons = [System.Windows.Forms.MessageBoxButtons]::OKCancel,
-        [System.Windows.Forms.MessageBoxIcon]$Icon = [System.Windows.Forms.MessageBoxIcon]::None
-    )
+﻿Add-Type -AssemblyName System.Windows.Forms
 
-    Add-Type -AssemblyName System.Windows.Forms
+$inputBox1 = New-Object System.Windows.Forms.TextBox
+$inputBox1.Location = New-Object Drawing.Point @(420, 25)
+$inputBox1.Size = New-Object Drawing.Size @(450, 2000)
+$inputBox1.Multiline = $false
+$inputBox1.Text = "C:\Users\Peng Yu\Downloads"
 
-    $inputBox1 = New-Object System.Windows.Forms.TextBox
-    $inputBox1.Location = New-Object Drawing.Point @(420, 25)
-    $inputBox1.Size = New-Object Drawing.Size @(450, 2000)
-    $inputBox1.Multiline = $false
-    $inputBox1.Text = $DefaultValue1
+$inputBox2 = New-Object System.Windows.Forms.TextBox
+$inputBox2.Location = New-Object Drawing.Point @(420, 245)
+$inputBox2.Size = New-Object Drawing.Size @(450, 2000)
+$inputBox2.Multiline = $false
+$inputBox2.Text = "2023-12-06"
 
-    $inputBox2 = New-Object System.Windows.Forms.TextBox
-    $inputBox2.Location = New-Object Drawing.Point @(420, 245)
-    $inputBox2.Size = New-Object Drawing.Size @(450, 2000)
-    $inputBox2.Multiline = $false
-    $inputBox2.Text = $DefaultValue2
+$form = New-Object Windows.Forms.Form
+$form.Text = "TechnologyCue"
+$form.Size = New-Object Drawing.Size @(950, 850)
 
-    $form = New-Object Windows.Forms.Form
-    $form.Text = $WindowTitle
-    $form.Size = New-Object Drawing.Size @(950, 850)
+$label1 = New-Object Windows.Forms.Label
+$label1.Location = New-Object Drawing.Point @(10, 20)
+$label1.Size = New-Object Drawing.Size @(400, 150)
+$label1.Text = "Output Path:"
 
-    $label1 = New-Object Windows.Forms.Label
-    $label1.Location = New-Object Drawing.Point @(10, 20)
-    $label1.Size = New-Object Drawing.Size @(400, 150)
-    $label1.Text = $Prompt1
+$label2 = New-Object Windows.Forms.Label
+$label2.Location = New-Object Drawing.Point @(10, 240)
+$label2.Size = New-Object Drawing.Size @(400, 150)
+$label2.Text = "End Date:"
 
-    $label2 = New-Object Windows.Forms.Label
-    $label2.Location = New-Object Drawing.Point @(10, 240)
-    $label2.Size = New-Object Drawing.Size @(400, 150)
-    $label2.Text = $Prompt2
+$form.Font = New-Object System.Drawing.Font("Arial", 26)  
+$label1.Font = New-Object System.Drawing.Font("Arial", 26)  
+$label2.Font = New-Object System.Drawing.Font("Arial", 26)  
+$inputBox1.Font = New-Object System.Drawing.Font("Arial", 26)  
+$inputBox2.Font = New-Object System.Drawing.Font("Arial", 26)  
 
-    $form.Font = New-Object System.Drawing.Font("Arial", 26)  
-    $label1.Font = New-Object System.Drawing.Font("Arial", 26)  
-    $label2.Font = New-Object System.Drawing.Font("Arial", 26)  
-    $inputBox1.Font = New-Object System.Drawing.Font("Arial", 26)  
-    $inputBox2.Font = New-Object System.Drawing.Font("Arial", 26)  
+$form.Controls.Add($label1)
+$form.Controls.Add($inputBox1)
+$form.Controls.Add($label2)
+$form.Controls.Add($inputBox2)
 
+$okButton = New-Object Windows.Forms.Button
+$okButton.Location = New-Object Drawing.Point @(90, 540)
+$okButton.Size = New-Object Drawing.Size @(300, 100)
+$okButton.Text = "OK"
+$okButton.DialogResult = [Windows.Forms.DialogResult]::OK
 
+$cancelButton = New-Object Windows.Forms.Button
+$cancelButton.Location = New-Object Drawing.Point @(490, 540)
+$cancelButton.Size = New-Object Drawing.Size @(300, 100)
+$cancelButton.Text = "Cancel"
+$cancelButton.DialogResult = [Windows.Forms.DialogResult]::Cancel
 
+$form.AcceptButton = $okButton
+$form.CancelButton = $cancelButton
 
+$form.Controls.Add($okButton)
+$form.Controls.Add($cancelButton)
 
-    $form.Controls.Add($label1)
-    $form.Controls.Add($inputBox1)
-    $form.Controls.Add($label2)
-    $form.Controls.Add($inputBox2)
+$form.Topmost = $true
 
-    $okButton = New-Object Windows.Forms.Button
-    $okButton.Location = New-Object Drawing.Point @(90, 540)
-    $okButton.Size = New-Object Drawing.Size @(300, 100)
-    $okButton.Text = "OK"
-    $okButton.DialogResult = [Windows.Forms.DialogResult]::OK
+$result = $form.ShowDialog()
 
-    $cancelButton = New-Object Windows.Forms.Button
-    $cancelButton.Location = New-Object Drawing.Point @(490, 540)
-    $cancelButton.Size = New-Object Drawing.Size @(300, 100)
-    $cancelButton.Text = "Cancel"
-    $cancelButton.DialogResult = [Windows.Forms.DialogResult]::Cancel
-
-    $form.AcceptButton = $okButton
-    $form.CancelButton = $cancelButton
-
-    $form.Controls.Add($okButton)
-    $form.Controls.Add($cancelButton)
-
-    $form.Topmost = $true
-
-    $result = $form.ShowDialog()
-
-    if ($result -eq [Windows.Forms.DialogResult]::OK) {
-        return $inputBox1.Text, $inputBox2.Text
-    } else {
-        return $null
-    }
+if ($result -eq [Windows.Forms.DialogResult]::OK) {
+    $userInput1 = $inputBox1.Text
+    $userInput2 = $inputBox2.Text
+} else {
+    Write-Host "User canceled the input."
 }
 
-
-$userInput1, $userInput2 = Read-InputBoxDialog -Prompt1 "Output Path:" -Prompt2 "End Date:" -DefaultValue1 "C:\Users\Peng Yu\Downloads" -DefaultValue2 "2023-12-06" -WindowTitle "TechnologyCue" -Buttons OKCancel -Icon Question
-
-
-<#
-    Description: This PowerShell script retrieves Power BI activity log events for 
-                 a specified date range and exports the results to a CSV file. 
-                 The script collects activity logs for each day within the 
-                 specified start and end dates.
-
-    Parameters:
-    - outputPath: The path where the CSV file will be saved. 
-    - startDate: The start date for retrieving activity logs. 
-    - endDate: The end date for retrieving activity logs. 
-#>
 
 $outputPath = $userInput1
 $endDate = $userInput2
